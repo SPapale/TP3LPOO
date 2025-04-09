2557366f10c5123a3a29543ab6d50414c40ee274
Santiago <Santiago@example.com>
2025-04-09 10:14:55 -0300

    Subo ejercicio 3

---
+++ ej3.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+
+int encontrarMayor(int *arr, int tam) {
+    int mayor = *arr;
+    for (int i = 1; i < tam; i++) {
+        if (*(arr + i) > mayor) {
+            mayor = *(arr + i);
+        }
+    }
+    return mayor;
+}
+
+
+int encontrarMenor(int *arr, int tam) {
+    int menor = *arr;
+    for (int i = 1; i < tam; i++) {
+        if (*(arr + i) < menor) {
+            menor = *(arr + i);
+        }
+    }
+    return menor;
+}
+
+int main() {
+    int tamanio;
+    int arreglo[100]; 
+
+    printf("Ingrese la cantidad de elementos del arreglo (máximo 100): ");
+    scanf("%d", &tamanio);
+
+    if (tamanio > 100 || tamanio <= 0) {
+        printf("Tamaño inválido.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < tamanio; i++) {
+        printf("Ingrese el elemento %d: ", i + 1);
+        scanf("%d", &arreglo[i]);
+    }
+
+    int mayor = encontrarMayor(arreglo, tamanio);
+    int menor = encontrarMenor(arreglo, tamanio);
+
+    printf("\nEl mayor elemento es: %d\n", mayor);
+    printf("El menor elemento es: %d\n", menor);
+
+    return 0;
+}
