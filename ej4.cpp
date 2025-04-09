5d9cc5a48164e88a9663176ec89194e8204ddf71
Santiago <Santiago@example.com>
2025-04-09 10:50:46 -0300

    Subo ejercicio 4

---
+++ ej4.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void copiarArreglo(int *origen, int *destino, int tamanio) {
+    for (int i = 0; i < tamanio; i++) {
+        *(destino + i) = *(origen + i); 
+    }
+}
+
+
+void mostrarArreglo(int *arr, int tamanio) {
+    for (int i = 0; i < tamanio; i++) {
+        printf("%d ", *(arr + i));
+    }
+    printf("\n");
+}
+
+int main() {
+    int tamanio;
+
+    printf("Ingrese la cantidad de elementos del arreglo (maximo 100): ");
+    scanf("%d", &tamanio);
+
+    if (tamanio > 100 || tamanio <= 0) {
+        printf("Tamanio invalido.\n");
+        return 1;
+    }
+
+    int arreglo1[100];
+    int arreglo2[100];
+
+
+    for (int i = 0; i < tamanio; i++) {
+        printf("Ingrese el elemento %d: ", i + 1);
+        scanf("%d", &arreglo1[i]);
+    }
+
+
+    copiarArreglo(arreglo1, arreglo2, tamanio);
+
+
+    printf("\nContenido del segundo arreglo (copia): ");
+    mostrarArreglo(arreglo2, tamanio);
+
+    return 0;
+}
